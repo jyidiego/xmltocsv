f984596293cdd8e78fb8a16fc69f693f55b098f0
John Yi <jyi@pivotal.io>
2017-05-14 16:19:59 -0400

    Updated to allow appsettings.json to configure RabbitMQ and updated stdout.

--- src/api/Services/QueueService.cs
+++ src/api/Services/QueueService.cs
@@ -1,83 +1,83 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using APIService.Handlers;
 using APIService.Models;
-using APIService.Repository;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using RabbitMQ.Client;
 
 namespace APIService.Services
 {
-  public class QueueService : IQueueService
-  {
-    private ConnectionFactory _connectionFactory;
-    private IQueueConsumerService _queueConsumerService;
-    private ILogger<QueueService> _logger; 
-    private Dictionary<string,IMessageHandler> _handlers;
-    public QueueService(IQueueConsumerService queueConsumerService, ConnectionFactory rabbitConnection, ILoggerFactory loggerFactory)
-    {
-       _connectionFactory = rabbitConnection;
-      
-      _queueConsumerService = queueConsumerService;
-      _logger = loggerFactory.CreateLogger<QueueService>();
+	public class QueueService : IQueueService
+	{
+		private ConnectionFactory _connectionFactory;
+		private IQueueConsumerService _queueConsumerService;
+		private ILogger<QueueService> _logger;
+		private Dictionary<string, IMessageHandler> _handlers;
+		public QueueService(IQueueConsumerService queueConsumerService, ConnectionFactory rabbitConnection, ILoggerFactory loggerFactory, IOptions<DataFlowServiceConfig> config)
+		{
+			_connectionFactory = rabbitConnection;
 
-      _queueConsumerService.QueueName = "test-queue";
-      _queueConsumerService.ExchangeName ="ExchangeName";
-      _queueConsumerService.ExchangeType = "direct";
-      _queueConsumerService.RoutingKeyName = string.Empty;
-      _queueConsumerService.Connect(_connectionFactory);
-      
-    }
+			_logger = loggerFactory.CreateLogger<QueueService>();
 
-    public void ProcessMessage(string message, IQueueConsumerService queueConsumerService, ulong deliveryTag, QueueMetric queueMetric)
-    {
-      var handlerFunc = ResolveHandler();
-      if(handlerFunc.Invoke(message))
-      {
-        queueConsumerService.Model.BasicAck(deliveryTag, false);
-        queueMetric.RoutingAction = RoutingAction.Processed;
-        return;
-      }
-     
-      this.RaiseException(new Exception("Message not processed."), queueConsumerService,deliveryTag,queueMetric);
-     
-    }
+			_queueConsumerService = queueConsumerService;
+			_queueConsumerService.QueueName = config.Value.InQueueName;
+			_queueConsumerService.ExchangeName = config.Value.ExchangeName;
+			_queueConsumerService.ExchangeType = config.Value.ExchangeType;
+			_queueConsumerService.RoutingKeyName = config.Value.InRoutingKeyName;
+			_queueConsumerService.Connect(_connectionFactory);
 
-    private void RaiseException(Exception ex, IQueueConsumerService queueConsumerService, ulong deliveryTag, QueueMetric queueMetric)
-    {
-      queueConsumerService.Model.BasicNack(deliveryTag, false, false);
-      queueMetric.RoutingAction = RoutingAction.Failed;
+		}
 
-      _logger.LogError($"Error raised from QueueService: {ex.Message}");
-    }
+		public void ProcessMessage(string message, IQueueConsumerService queueConsumerService, ulong deliveryTag, QueueMetric queueMetric)
+		{
+			var handlerFunc = ResolveHandler();
+			if (handlerFunc.Invoke(message))
+			{
+				queueConsumerService.Model.BasicAck(deliveryTag, false);
+				queueMetric.RoutingAction = RoutingAction.Processed;
+				return;
+			}
 
-    public void ProcessQueue()
-    {
-      _queueConsumerService.ReadFromQueue(ProcessMessage, RaiseException,_queueConsumerService.ExchangeName,
-        _queueConsumerService.QueueName,_queueConsumerService.RoutingKeyName);
-    }
+			this.RaiseException(new Exception("Message not processed."), queueConsumerService, deliveryTag, queueMetric);
 
-    public void RegisterHandler(IMessageHandler handler)
-    {
-      if(_handlers == null)
-        _handlers = new Dictionary<string, IMessageHandler>();
+		}
 
-      _handlers.Add("myHandler",handler);
-    }
+		private void RaiseException(Exception ex, IQueueConsumerService queueConsumerService, ulong deliveryTag, QueueMetric queueMetric)
+		{
+			queueConsumerService.Model.BasicNack(deliveryTag, false, false);
+			queueMetric.RoutingAction = RoutingAction.Failed;
 
-    public void RegisterHandlers(IEnumerable<IMessageHandler> handlers)
-    {
-      foreach(var h in handlers)
-      {
-        RegisterHandler(h);
-      }
-    }
+			_logger.LogError($"Error raised from QueueService: {ex.Message}");
+		}
 
-    private Func<string, bool> ResolveHandler()
-    {
-      var m = _handlers["myHandler"];    
-      return m.Handle;  
-    }
-    
-  }
+		public void ProcessQueue()
+		{
+			_queueConsumerService.ReadFromQueue(ProcessMessage, RaiseException, _queueConsumerService.ExchangeName,
+			  _queueConsumerService.QueueName, _queueConsumerService.RoutingKeyName);
+		}
+
+		public void RegisterHandler(IMessageHandler handler)
+		{
+			if (_handlers == null)
+				_handlers = new Dictionary<string, IMessageHandler>();
+
+			_handlers.Add("myHandler", handler);
+		}
+
+		public void RegisterHandlers(IEnumerable<IMessageHandler> handlers)
+		{
+			foreach (var h in handlers)
+			{
+				RegisterHandler(h);
+			}
+		}
+
+		private Func<string, bool> ResolveHandler()
+		{
+			var m = _handlers["myHandler"];
+			return m.Handle;
+		}
+
+	}
 }
--- src/api/Startup.cs
+++ src/api/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using APIService.Extensions;
+﻿using APIService.Extensions;
 using APIService.Handlers;
 using APIService.Repository;
 using APIService.Services;
@@ -11,7 +7,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
 
 using Steeltoe.CloudFoundry.Connector.Rabbit;
 using Steeltoe.CloudFoundry.Connector.Redis;
@@ -22,83 +17,82 @@ using APIService.Models;
 
 namespace APIService
 {
-    public class Startup
-    {
-        public Startup(IHostingEnvironment env, ILoggerFactory logFactory)
-        {
-            logFactory.AddConsole(minLevel: LogLevel.Debug);
+	public class Startup
+	{
+		public Startup(IHostingEnvironment env, ILoggerFactory logFactory)
+		{
+			logFactory.AddConsole(minLevel: LogLevel.Debug);
 
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables()
-                .AddCloudFoundry();
-            Configuration = builder.Build();
-        }
+			var builder = new ConfigurationBuilder()
+			    .SetBasePath(env.ContentRootPath)
+			    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+			    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+			    .AddEnvironmentVariables()
+			    .AddCloudFoundry();
+			Configuration = builder.Build();
+		}
 
-        public IConfigurationRoot Configuration { get; }
+		public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-             // Add framework services.
-            services.AddMvc();
-            
-            services.AddRedisConnectionMultiplexer(Configuration);
-            services.AddRabbitConnection(Configuration);
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			// Add framework services.
+			services.AddMvc();
 
-            // Add Context and use MySql as provider ... provider will be configured from VCAP_ info
-            services.AddDbContext<OrdersContext>(options => options.UseMySql(Configuration));
+			services.AddRedisConnectionMultiplexer(Configuration);
+			services.AddRabbitConnection(Configuration);
 
-            services.AddSwaggerGen();
+			services.AddSwaggerGen();
 
-            services.ConfigureSwaggerGen(options =>
-            {
-                //options.AddSecurityDefinition("IdentityServer", sch);
-                //options.OperationFilter<AuthorizationHeaderParameterOperationFilter>();
-                options.DescribeAllEnumsAsStrings();
-                options.SingleApiVersion(new Swashbuckle.Swagger.Model.Info()
-                {
-                    Title = "Example API Service HTTP API",
-                    Version = "v1",
-                    Description = "The API Service HTTP API",
-                    TermsOfService = "Terms Of Service"
-                });
-            });
+			services.ConfigureSwaggerGen(options =>
+			{
+		    //options.AddSecurityDefinition("IdentityServer", sch);
+		    //options.OperationFilter<AuthorizationHeaderParameterOperationFilter>();
+		    options.DescribeAllEnumsAsStrings();
+				options.SingleApiVersion(new Swashbuckle.Swagger.Model.Info()
+				{
+					Title = "Example API Service HTTP API",
+					Version = "v1",
+					Description = "The API Service HTTP API",
+					TermsOfService = "Terms Of Service"
+				});
+			});
 
-            services.AddCors(options =>
-            {
-                options.AddPolicy("CorsPolicy",
-                    builder => builder.AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials());
-            });
+			services.AddCors(options =>
+			{
+				options.AddPolicy("CorsPolicy",
+			builder => builder.AllowAnyOrigin()
+			.AllowAnyMethod()
+			.AllowAnyHeader()
+			.AllowCredentials());
+			});
 
-            services.AddTransient<IMetricsRepository, RedisMetricsRepository>();
-            services.AddTransient<IMessageHandler, XmlToCsvHandler>();
-            services.AddSingleton<IQueueConsumerService, QueueConsumerService>();
-            services.AddSingleton<IQueueProducerService, QueueProducerService>();
-            services.AddSingleton<IQueueService, QueueService>();            
-        }
+			services.AddOptions();
+			services.Configure<DataFlowServiceConfig>(Configuration.GetSection("DataFlowServiceConfig"));
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
 
-            app.StartQueueService();
-            
-            app.UseCors("CorsPolicy");
+			services.AddTransient<IMetricsRepository, RedisMetricsRepository>();
+			services.AddSingleton<IMessageHandler, XmlToCsvHandler>();
+			services.AddSingleton<IQueueConsumerService, QueueConsumerService>();
+			services.AddSingleton<IQueueProducerService, QueueProducerService>();
+			services.AddSingleton<IQueueService, QueueService>();
+		}
 
-            app.UseMvcWithDefaultRoute();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+			loggerFactory.AddDebug();
 
-            app.UseSwagger()
-                .UseSwaggerUi();
+			app.StartQueueService();
 
-            SampleData.InitializeOrderDatabase(app.ApplicationServices);
-        }
-    }
+			app.UseCors("CorsPolicy");
+
+			app.UseMvcWithDefaultRoute();
+
+			app.UseSwagger()
+			    .UseSwaggerUi();
+		}
+	}
 }
